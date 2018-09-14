bde76196424306d0a4c58602dc1913fff5b42cb0
Fabio F. Destro <fbfdestro@gmail.com>
2018-09-14 20:43:52 -0300

    Javascript add elements

--- popup/popScript.js
+++ popup/popScript.js
@@ -1,11 +1,37 @@
 function sendMsg(msg){
-	chrome.tabs.query({active: true, currentWindow: true},pegouTab);
+/*	chrome.tabs.query({active: true, currentWindow: true},pegouTab);
 	function pegouTab(tabs){
 		chrome.tabs.sendMessage(tabs[0].id, msg);
 	}
+	*/
 }
 
-function inserirMenu(texto,id){
+function inserirMenu(id,nomeImg,texto){
+
+	var divBtn = document.createElement("div");
+	divBtn.setAttribute("id",id);
+	divBtn.setAttribute("class","btn");
+
+	var divIcone = document.createElement("div");
+	divIcone.setAttribute("class", "icone");
+
+	var img = document.createElement("img");
+	img.src = "../imgs/"+nomeImg+".png";
+
+	var link = document.createElement("a");
+	link.href = "#";
+	link.innerText = texto;
+
+	divIcone.appendChild(img);
+	divBtn.appendChild(divIcone);
+	divBtn.appendChild(link);
+
+	console.log(botoes);
+	botoes.prepend(divBtn); // não funciona em versões antigas
+	return divBtn;
+
+
+	/*
 	var liElement = document.createElement("li");
 	var aElement = document.createElement("a");
 	aElement.href = "#";
@@ -14,6 +40,7 @@ function inserirMenu(texto,id){
 	liElement.appendChild(aElement);
 	menu.prepend(liElement);
 	return liElement;
+	*/
 }
 
 function exibeExistenteMenu(elemeto){
@@ -63,9 +90,21 @@ window.onload = function(){
 
 	sendMsg("infoRequest");
 
+	botoes = document.getElementById("botoes");
+	console.log(botoes);
+
+	/*
+
 	menu = document.getElementById("opcoes");
 	var confExt = inserirMenu("Configurar extensão","confExt");
 	var loadExt = inserirMenu("Carregando informações","loadExt");
+
+	*/
+
+	botoes.prepend(document.createElement("hr"));
+	var btnAddDislike = inserirMenu("btnDislike","dislike","Sempre dar dislike nesse canal");
+	var btnAddLike = inserirMenu("btnLike","like","Sempre dar like nesse canal");
+
 
 //	var btnAddLike = document.getElementById("addLike");
 //	var btnAddDislike = document.getElementById("addDislike");
