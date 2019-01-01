71dc21b287349bf5fc5423ad8a6f15e52bd4e82b
FbFDestro <fbfdestro@gmail.com>
2019-01-01 02:31:48 -0200

    add buttons iterating the lists

--- pages/configuration.js
+++ pages/configuration.js
@@ -3,21 +3,11 @@ class Page{
         this.slider = document.getElementById('myRange');
         this.whenReact = document.getElementsByClassName('percent')[0];
         this.sliderStartValue = undefined;
-        this.getSliderValue();
-
         this.likeList = undefined;
         this.dislikeList = undefined;
-        this.getListsValue();
-
+        this.likeContent = document.querySelector('.like > .content-box');
+        this.dislikeContent = document.querySelector('.dislike > .content-box');
         this.saveBtn = document.querySelector('.save > .content-box > .btn');
-    }
-
-    getSliderValue(){
-        getSliderValue(this);
-    }
-
-    setSliderValue(){
-        setSliderValue(this);
     }
 
     getListsValue(){
@@ -30,34 +20,61 @@ class Page{
 
 }
 
-function getSliderValue(thePage){
+function getSliderValue(){
     chrome.storage.sync.get(['whenReactInPercent'], function (result) {
-        thePage.sliderStartValue = result.whenReactInPercent;
-        thePage.setSliderValue();
+        page.sliderStartValue = result.whenReactInPercent;
+        setSliderValue();
     });
 }
 
-function getListsValue(thePage){
-    chrome.storage.sync.get(null, (result) => {
-        thePage.likeList = result.likeList;
-        thePage.dislikeList = result.dislikeList;
-
-        console.log('Lista de likes: ' + result.likeList);
-        console.log('Lista de dislikes: ' + result.dislikeList);
-    });
-}
-
-
-function setSliderValue(thePage) {
-    console.log(thePage.sliderStartValue);
-    thePage.slider.value = thePage.sliderStartValue*100;
-    thePage.whenReact.innerHTML = thePage.slider.value + '%';
-    thePage.slider.oninput = function () {
-        thePage.whenReact.innerText = thePage.slider.value + '%';
-        if(thePage.slider.value != thePage.sliderStartValue){
-            thePage.setChangesToBeSaved();
+function setSliderValue() {
+    console.log(page.sliderStartValue);
+    page.slider.value = page.sliderStartValue*100;
+    page.whenReact.innerHTML = page.slider.value + '%';
+    page.slider.oninput = function () {
+        page.whenReact.innerText = page.slider.value + '%';
+        if(page.slider.value != page.sliderStartValue){
+            page.setChangesToBeSaved();
         }
     }
 }
 
+function getListsValue(){
+    chrome.storage.sync.get(null, (result) => {
+        page.likeList = result.likeList;
+        page.dislikeList = result.dislikeList;
+
+        console.log('Lista de likes: ' + page.likeList);
+        console.log('Lista de dislikes: ' + page.dislikeList);
+        
+        iterateListCreatingButtons(page.likeList, page.likeContent);
+        iterateListCreatingButtons(page.dislikeList, page.dislikeContent);
+    });
+}
+
+function iterateListCreatingButtons(list, content){
+    const addSrc = '../imgs/add.png';
+    const removeSrc = '../imgs/remove.png';
+     if (list !== undefined) {
+        for (let i = 0; i < list.length; i++) {
+            let btn = document.createElement('div');
+            btn.classList.add('btn');
+            btn.innerHTML = '<p>' + list[i].slice(0, -1) + '</p><img src="' + removeSrc + '">';
+            btn.onclick = function () {
+                let icon = btn.querySelector('img');
+                if (icon.src.endsWith(removeSrc.substr(2, removeSrc.length))) {
+                    icon.src = addSrc;
+                } else {
+                    icon.src = removeSrc;
+                }
+                btn.classList.toggle('removed');
+            }
+            content.append(btn);
+        }
+    }
+}
+
+
 let page = new Page();
+getSliderValue();
+getListsValue();
