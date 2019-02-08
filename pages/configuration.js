7c1f049a7bad4b5d269f019a2449d12d62420b91
FbFDestro <fbfdestro@gmail.com>
2019-02-08 14:22:55 -0200

    adding analytics

--- pages/configuration.js
+++ pages/configuration.js
@@ -182,3 +182,17 @@ function removeButtons(content) {
 let page = new Page();
 getSliderValue();
 getListsValue();
+
+var _AnalyticsCode = 'UA-134153603-1';
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', _AnalyticsCode]);
+_gaq.push(['_trackPageview']);
+
+(function () {
+    var ga = document.createElement('script');
+    ga.type = 'text/javascript';
+    ga.async = true;
+    ga.src = 'https://ssl.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ga, s);
+})();
--- popup/popup.js
+++ popup/popup.js
@@ -184,3 +184,33 @@ function refreshed() {
     }, 600);
 
 }
+
+var _AnalyticsCode = 'UA-134153603-1';
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', _AnalyticsCode]);
+_gaq.push(['_trackPageview']);
+
+(function () {
+    var ga = document.createElement('script');
+    ga.type = 'text/javascript';
+    ga.async = true;
+    ga.src = 'https://ssl.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ga, s);
+})();
+
+function trackButtonClick(e) {
+    _gaq.push(['_trackEvent', e.target.className, 'clicked']);
+    console.log("id " + e.target.className);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    itens = ['support', 'subscribe', 'refresh', 'alwaysLike', 'alwaysDislike', 'configureExtensionBtn'];
+    for (item of itens) {
+        let button = document.getElementById(item);
+        if (button !== null && button !== undefined) {
+            button.addEventListener('click', trackButtonClick);
+            console.log(button);
+        }
+    }
+});
