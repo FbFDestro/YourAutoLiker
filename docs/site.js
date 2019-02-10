ac108e9a163cb021b84e936c5844501f02b1e0d1
FbFDestro <fbfdestro@gmail.com>
2019-02-10 03:23:04 -0200

    analytics events test on website

--- docs/site.js
+++ docs/site.js
@@ -13,10 +13,23 @@ if (hash == '#thanks' || hash == '#share') {
 document.querySelector('#messagePop > img.closeImg').onclick = function () {
     document.getElementById('messagePop').setAttribute('show', 'false');
 }
+var _AnalyticsCode = 'UA-134153603-2';
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', _AnalyticsCode]);
+_gaq.push(['_trackPageview']);
+
+(function () {
+    var ga = document.createElement('script');
+    ga.type = 'text/javascript';
+    ga.async = true;
+    ga.src = 'https://ssl.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ga, s);
+})();
 
 function trackButtonClick(e) {
-    ga('send', 'event', e.target.className, 'clicked');
-    /*     _gaq.push(['_trackEvent', e.target.className, 'clicked']); */
+    /*     ga('send', 'event', e.target.className, 'clicked'); */
+    _gaq.push(['_trackEvent', e.target.className, 'clicked']);
     console.log(' class ' + e.target.className);
 }
 
