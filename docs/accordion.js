28da4241ed95f8bc106415f25b78e35451649fa8
FbFDestro <fbfdestro@gmail.com>
2020-02-28 00:22:44 -0300

    bilingual website support en/pt

--- docs/accordion.js
+++ docs/accordion.js
@@ -1,35 +1,33 @@
-var acc = document.getElementsByClassName("accordion");
+var acc = document.getElementsByClassName('accordion');
 
 for (let i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function () {
-        closeAll(i);
-        this.classList.toggle("active");
-        let panel = this.nextElementSibling;
-        panel.classList.toggle("open");
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-        }
-    });
+  acc[i].addEventListener('click', function() {
+    closeAll(i);
+    this.classList.toggle('active');
+    let panel = this.nextElementSibling;
+    panel.classList.toggle('open');
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + 'px';
+    }
+  });
 }
 
 function closeAll(atual) {
-    for (let i = 0; i < acc.length; i++) {
-        if (i != atual) {
-            acc[i].classList.remove("active");
-            let panel = acc[i].nextElementSibling;
-            panel.classList.remove("open");
-            panel.style.maxHeight = null;
-        }
+  for (let i = 0; i < acc.length; i++) {
+    if (i != atual) {
+      acc[i].classList.remove('active');
+      let panel = acc[i].nextElementSibling;
+      panel.classList.remove('open');
+      panel.style.maxHeight = null;
     }
+  }
 }
 
-
-if (window.location.hash == '#contato') {
-    acc[acc.length - 1].click();
-    document.getElementById('messagePop').setAttribute('show', 'true');
-
+if (window.location.hash == '#contato' || window.location.hash == '#contact') {
+  acc[acc.length - 1].click();
+  document.getElementById('messagePop').setAttribute('show', 'true');
 } else {
-    acc[0].click();
+  acc[0].click();
 }
--- docs/site.js
+++ docs/site.js
@@ -1,12 +1,11 @@
 let hash = window.location.hash;
 if (hash == '#thanks' || hash == '#share') {
-
-    document.getElementById('messagePop').setAttribute('show', 'true');
-    if (hash == '#thanks') {
-        document.querySelector('#messagePop > p.thanks').setAttribute('show', 'true');
-    } else {
-        document.querySelector('#messagePop > p.share').setAttribute('show', 'true');
-    }
+  document.getElementById('messagePop').setAttribute('show', 'true');
+  if (hash == '#thanks') {
+    document.querySelector('#messagePop > p.thanks').setAttribute('show', 'true');
+  } else {
+    document.querySelector('#messagePop > p.share').setAttribute('show', 'true');
+  }
 }
 
 /*
@@ -20,34 +19,33 @@ window.onhashchange = () => {
 }
 */
 
-document.querySelector('#messagePop > img.closeImg').onclick = function () {
-    document.getElementById('messagePop').setAttribute('show', 'false');
-}
+document.querySelector('#messagePop > img.closeImg').onclick = function() {
+  document.getElementById('messagePop').setAttribute('show', 'false');
+};
 
-let menuMobile = document.getElementById("menuMobileOpen");
-menuMobile.onclick = function () {
-    let menuIcon = this.innerText;
-    let nav = document.getElementsByTagName("nav")[0];
-    if (menuIcon == "☰ ＋") {
-        nav.classList.remove('hideMobile');
-        this.innerText = "☰ ➖";
-    } else {
-        nav.classList.add('hideMobile');
-        this.innerText = "☰ ＋";
-    }
-
+let menuMobile = document.getElementById('menuMobileOpen');
+menuMobile.onclick = function() {
+  let menuIcon = this.innerText;
+  let nav = document.getElementsByTagName('nav')[0];
+  if (menuIcon == '☰ ＋') {
+    nav.classList.remove('hideMobile');
+    this.innerText = '☰ ➖';
+  } else {
+    nav.classList.add('hideMobile');
+    this.innerText = '☰ ＋';
+  }
 };
 
 function menuReact(e) {
-    let menuIcon = menuMobile.innerText;
-    if (menuIcon == "☰ ➖") {
-        let nav = document.getElementsByTagName("nav")[0];
-        nav.classList.add('hideMobile');
-        menuMobile.innerText = "☰ ＋";
-    }
+  let menuIcon = menuMobile.innerText;
+  if (menuIcon == '☰ ➖') {
+    let nav = document.getElementsByTagName('nav')[0];
+    nav.classList.add('hideMobile');
+    menuMobile.innerText = '☰ ＋';
+  }
 }
 
 let menu = document.getElementById('menu');
 for (item of menu.getElementsByTagName('li')) {
-    item.addEventListener('click', menuReact);
+  item.addEventListener('click', menuReact);
 }
